5b94b1efd516c24a4f1e41c200b805517c70d3af
go-me <47760572+go-me@users.noreply.github.com>
2019-02-19 04:01:11 +0300

    Create me.go

---
+++ me.go
@@ -0,0 +1,28 @@
+package me
+
+import "fmt"
+
+var(
+Greeting string = "Good Morrow! You wake me up!"
+
+
+)
+
+func main() {
+	fmt.Println(Greeting)
+	fmt.Println("I really got excited and giddy when I saw your advertisement.")
+	fmt.Println("Firstly, I don't have any University degree in Computer Science or any equivalent department.")
+	fmt.Println("If you care about it, you can go ahead.")
+	fmt.Println("")
+	fmt.Println("But if you don't care, then I have some more valuable things for you.")
+	fmt.Println("Firstly, I'm a learning machine. I can learn anything fast, clear and long-term.")
+	fmt.Println("Secondly, I'm persistent more than enough. I can stay awake more than 24 hours to debug a bug.")
+	fmt.Println("Thirdly, I'm young, a raw material. You can tilt, twist, shape and color how you wanted. (22 years old)")
+	fmt.Println("Last and most importantly, I'm a lover of coding. I don't want to write codes, I want to write poetry.")
+	fmt.Println("")
+	fmt.Println("I don't know if you care about but...")
+	fmt.Println("I learned this language just in 2 hours to write this message and make a reference as you did. :)")
+	fmt.Println("Bonus: I don't stop till achieve.")
+	fmt.Println("")
+	fmt.Println("Doğan Kırnaz - kirnazdogan@gmail.com")
+}
